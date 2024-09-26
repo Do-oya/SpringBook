c5a268a2db666fcde765b7e07f8aa98c5993ea31
Do-oya <qkrwjdgus1441@naver.com>
2024-09-26 14:34:42 +0900

    feat: 스프링 IoC

--- src/main/java/toby/springbook/user/dao/DaoFactory.java
+++ src/main/java/toby/springbook/user/dao/DaoFactory.java
@@ -1,14 +1,19 @@
 package toby.springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import toby.springbook.user.domain.ConnectionMaker;
 import toby.springbook.user.domain.DConnectionMaker;
 import toby.springbook.user.domain.UserDao;
 
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
         return new UserDao(connectionMaker());
     }
 
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new DConnectionMaker();
     }
--- src/main/java/toby/springbook/user/domain/UserDaoTest.java
+++ src/main/java/toby/springbook/user/domain/UserDaoTest.java
@@ -1,15 +1,18 @@
 package toby.springbook.user.domain;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import toby.springbook.user.dao.DaoFactory;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
-        user.setId("qwerr");
+        user.setId("q");
         user.setName("정현");
         user.setPassword("1234");
 
